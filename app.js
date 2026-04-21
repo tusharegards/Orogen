91e8868756f0c2804ca6825f90404d6b79dd36dc
Tushar <tushardews@gmail.com>
2026-04-21 12:59:02 +0530

    Client Website Launch v1

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 import express from 'express';
 import path from 'path';
-import loanRoutes from './routes/loan.routes.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-//set the view engine to ejs
-app.set('view engine', 'ejs');
-//to serve static files
-const assets = path.join(path.resolve(), 'assets');
-//set middleware to serve static files
-app.use('/assets',express.static(assets))
-app.set('views', path.join(path.resolve(), 'views'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));   
 
-// Set the document views directory
-app.use('/document', loanRoutes);
 
 //default Home Page
 app.get('/', (req, res) => {
-    res.render("omnitix");
+    res.render("orogen");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
